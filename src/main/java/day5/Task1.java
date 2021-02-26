1783928613fe053eae8a2bd11ef3e2a08fa56aad
Andrei Murashkin <GoodKid88@yandex.ru>
2021-02-26 17:44:59 +0200

    add day5 solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,41 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setYear(2002);
+        car.setColor("Black");
+        car.setModel("BMW 520");
 
+        System.out.println(car.getYear() + " " + car.getColor() + " " + car.getModel());
+    }
+}
+
+class Car {
+    private int year;
+    private String color;
+    private String model;
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 }
